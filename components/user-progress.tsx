f9d5d4acae4b0ae3d5a94954bbb0d036e30f370f
nuhptr <nugrahaadi733@gmail.com>
2024-03-20 01:27:06 +0700

    update props and formatting import

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link"
 import Image from "next/image"
-
-import { Button } from "@/components/ui/button"
 import { InfinityIcon } from "lucide-react"
 
+import { Button } from "@/components/ui/button"
+
+import { courses } from "@/db/schema"
+
 type Props = {
-    activeCourse: { imageSrc: string; title: string } // later change with DB type
+    activeCourse: typeof courses.$inferSelect
     hearts: number
     points: number
     hasActiveSubscription: boolean
