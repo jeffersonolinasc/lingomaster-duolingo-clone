f23f578e55bf24ecf5c70b15391bd7efd6c97f29
nuhptr <nugrahaadi733@gmail.com>
2024-03-20 16:22:39 +0700

    loading: formatting

--- app/(main)/learn/loading.tsx
+++ app/(main)/learn/loading.tsx
@@ -4,8 +4,8 @@ import { Loader } from "lucide-react"
 
 export default function Loading() {
     return (
-        <div className="h-full w-full flex items-center justify-center">
-            <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
+        <div className="flex items-center justify-center w-full h-full">
+            <Loader className="w-6 h-6 text-muted-foreground animate-spin" />
         </div>
     )
 }
