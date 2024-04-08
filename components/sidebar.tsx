a7efb08caa1a32776f78da06c71df8fdd9500773
nuhptr <nugrahaadi733@gmail.com>
2024-04-08 15:31:12 +0700

    component: sidebar update code

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -10,7 +10,7 @@ type Props = {
     className?: string
 }
 
-export const Sidebar = ({ className }: Props) => {
+export function Sidebar({ className }: Props) {
     return (
         <div
             className={cn(
@@ -20,7 +20,12 @@ export const Sidebar = ({ className }: Props) => {
             )}>
             <Link href={"/learn"}>
                 <div className="flex items-center pt-8 pl-4 pb-7 gap-x-3">
-                    <Image src={"/ic_mascot.svg"} height={40} width={40} alt="mascot of lingomaster" />
+                    <Image
+                        src={"/ic_mascot.svg"}
+                        height={40}
+                        width={40}
+                        alt="mascot of lingomaster"
+                    />
                     <h1 className="text-2xl font-extrabold tracking-wide text-green-600">Lingo</h1>
                 </div>
             </Link>
@@ -28,7 +33,11 @@ export const Sidebar = ({ className }: Props) => {
             {/* flex-1 is growth size if needed */}
             <div className="flex flex-col flex-1 gap-y-2">
                 <SidebarItem label="Learn" href="/learn" iconSrc="/ic_learn.svg" />
-                <SidebarItem label="Leaderboard" href="/leaderboard" iconSrc="/ic_leaderboard.svg" />
+                <SidebarItem
+                    label="Leaderboard"
+                    href="/leaderboard"
+                    iconSrc="/ic_leaderboard.svg"
+                />
                 <SidebarItem label="Quests" href="/quests" iconSrc="/ic_quests.svg" />
                 <SidebarItem label="Shop" href="/shop" iconSrc="/ic_shop.svg" />
             </div>
